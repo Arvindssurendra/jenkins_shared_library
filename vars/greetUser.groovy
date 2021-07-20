4f492b5345a53522012f8c74af8dd5a0ae30973a
jaintpharsha <40075520+jaintpharsha@users.noreply.github.com>
2021-07-20 22:16:13 +0530

    Update greetUser.groovy

--- vars/greetUser.groovy
+++ vars/greetUser.groovy
@@ -1,16 +1,19 @@
-def call(String username = 'null') { 
+def call(String username = 'null', String designation = 'null') { 
 	
 	echo "This is shared library greetUser"
+	echo "${designation}"
 	
 	pipeline {
 		environment { 
 			USER_NAME = "${username}"
+			desig = "${designation}"
 		}	
 		agent any 
 		stages {
 			stage('GREET') {
 				steps {
 					echo "Hi ${USER_NAME}, How are you !"
+					echo "Your designation is ${desig}"
 				}
 			}
 		}
