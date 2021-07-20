746c7063a3d0111cdc3d94624e0a601532eb94a1
jaintpharsha <40075520+jaintpharsha@users.noreply.github.com>
2021-07-20 22:18:36 +0530

    Update greetUser.groovy

--- vars/greetUser.groovy
+++ vars/greetUser.groovy
@@ -1,4 +1,4 @@
-def call(String username = 'null', String designation = null) { 
+def call(String username = 'null', String designation) { 
 	
 	echo "This is shared library greetUser"
 	echo "${designation}"
